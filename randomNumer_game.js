5680f4ff35956bd2d42bac026480c5d74b045478
Gowtham07070 <157710020+Gowtham07070@users.noreply.github.com>
2025-02-27 19:45:37 +0530

    Create randomNumer_game.js

---
+++ randomNumer_game.js
@@ -0,0 +1,29 @@
+let input=prompt("enter your input");
+
+let randomNum=Math.floor(Math.random()*input)+1;
+console.log(randomNum);
+
+let ans=prompt("enter your guess");
+
+while(true){
+    
+    if(ans=="quit"){
+        console.log("you quit")
+        break;
+    }
+
+    if(ans==randomNum){
+        console.log("yes u won");
+        break;
+       
+    }else if(ans!=randomNum){
+        console.log("try again")
+        if(ans> randomNum){
+            console.log("try a smaller  number");
+        }else{
+            console.log("try a bigger number");
+        }
+    ans=prompt("enter your guess");
+    }
+
+}
